88d2c26f1a72aaa9042e38e46881ed98a79e7f19
fabian <fabianjuliansyah89@gmail.com>
2026-04-07 07:06:18 +0700

    Hotfix/Create migration for add nomor surat column in agenda_rapat table

---
+++ database/migrations/2026_04_07_000251_create_nomor_surat_column_in_agenda_rapat_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table("agenda_rapat", function (Blueprint $table) {
+            $table
+                ->string("nomor_surat")
+                ->nullable()
+                ->after("user_id")
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table("surat_pengiriman_barang", function (Blueprint $table) {
+            $table->dropColumn("nomor_surat");
+        });
+    }
+};
