90219f918e289f59f28162a8b13b5e539983c2ba
fabian <fabianjuliansyah89@gmail.com>
2026-05-18 14:46:22 +0700

    hotfix/Set comment to drop_unique_nomor_pesanan_pembelian migration

--- database/migrations/2026_05_05_150921_drop_unique_nomor_pesanan_pembelian_from_pesanan_pembelian_table.php
+++ database/migrations/2026_05_05_150921_drop_unique_nomor_pesanan_pembelian_from_pesanan_pembelian_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('pesanan_pembelian', function (Blueprint $table) {
-            $table->dropUnique('pesanan_pembelian_nomor_pesanan_pembelian_unique');
+            // $table->dropUnique('pesanan_pembelian_nomor_pesanan_pembelian_unique');
         });
     }
 
@@ -22,7 +21,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('pesanan_pembelian', function (Blueprint $table) {
-            $table->unique('nomor_pesanan_pembelian');
+            // $table->unique('nomor_pesanan_pembelian');
         });
     }
 };
